7c03fd13acaf7e7b81b3413f230fe6333cc05362
kolibriconk <36661173+kolibriconk@users.noreply.github.com>
2021-11-23 21:31:42 +0100

    Added date fileter when selecting it

--- lib/page_report.dart
+++ lib/page_report.dart
@@ -8,145 +8,212 @@ class PageReport extends StatefulWidget {
   _PageReportState createState() => _PageReportState();
 }
 
+DateTime _today = DateTime.now();
+
 class _PageReportState extends State<PageReport> {
-  String _periodSelected = "Last week";
   var periodItems = ["Last week", "This week", "Yesterday", "Today", "Other"];
-
-  String _contentSelected = "Brief";
   var contentItems = ["Brief", "Detailed", "Statistic"];
-
-  String _formatSelected = "WebPage";
   var formatItems = ["WebPage", "PDF", "Text"];
 
-  String _fromDate = DateFormat("yyyy-MM-dd").format(DateTime.now());
+  late String _periodSelected;
+  late String _formatSelected;
+  late String _contentSelected;
+  late DateTimeRange _dateRange;
+  late DateFormat _dateFormat;
 
-  String _toDate = DateFormat("yyyy-MM-dd").format(DateTime.now().add(const Duration(days: 1)));
+  @override
+  void initState() {
+    _dateFormat = DateFormat("yyyy-MM-dd");
+    _dateRange = DateTimeRange(start: _today, end: _today);
+    _contentSelected = contentItems[0];
+    _formatSelected = formatItems[0];
+    _periodSelected = periodItems[0];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Report'),
+          title: const Text('Report'),
         ),
         body: Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      child: const Text("Period "),
-                    ),
-                    DropdownButton(
-                      value: _periodSelected,
-                      items: periodItems.map((String periodItems) {
-                        return DropdownMenuItem(
-                            value: periodItems, child: Text(periodItems));
-                      }).toList(),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          _periodSelected = newValue!;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      child: const Text("From "),
-                    ),
-                    Container(
-                      child: Row(children: [
-                        Text(_fromDate),
-                        const Text("  "),
-                        IconButton(
-                          icon: Icon(Icons.calendar_today_outlined),
-                          highlightColor: Colors.cyan,
-                          color: Colors.cyan,
-                          onPressed: () {},
-                        )
-                      ]),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      child: Text("To "),
-                    ),
-                    Container(
-                      child: Row(children: [
-                        Text(_toDate),
-                        const Text("  "),
-                        IconButton(
-                          icon: Icon(Icons.calendar_today_outlined),
-                          highlightColor: Colors.cyan,
-                          color: Colors.cyan,
-                          onPressed: () {},
-                        )
-                      ]),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      child: Text("Content "),
-                    ),
-                    DropdownButton(
-                      value: _contentSelected,
-                      items: contentItems.map((String contentItems) {
-                        return DropdownMenuItem(
-                            value: contentItems, child: Text(contentItems));
-                      }).toList(),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          _contentSelected = newValue!;
-                        });
-                      },
-                    )
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      child: Text("Format "),
-                    ),
-                    DropdownButton(
-                      value: _formatSelected,
-                      items: formatItems.map((String formatItems) {
-                        return DropdownMenuItem(
-                            value: formatItems, child: Text(formatItems));
-                      }).toList(),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          _formatSelected = newValue!;
-                        });
-                      },
-                    )
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                        child: TextButton(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    child: const Text("Period"),
+                    width: 100,
+                    margin: const EdgeInsets.only(left: 80),
+                  ),
+                  DropdownButton(
+                    value: _periodSelected,
+                    items: periodItems.map((String periodItems) {
+                      return DropdownMenuItem(
+                          value: periodItems, child: Text(periodItems));
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _periodSelected = newValue!;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              Row(
+                // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    child: const Text("From"),
+                    width: 100,
+                    margin: const EdgeInsets.only(left: 80),
+                  ),
+                  Text(_dateFormat.format(_dateRange.start)),
+                  IconButton(
+                    icon: const Icon(Icons.calendar_today_outlined),
+                    highlightColor: Colors.cyan,
+                    color: Colors.cyan,
+                    onPressed: () {
+                      _pickFromDate();
+                    },
+                  )
+                ],
+              ),
+              Row(
+                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    child: const Text("To"),
+                    width: 100,
+                    margin: const EdgeInsets.only(left: 80),
+                  ),
+                  Text(_dateFormat.format(_dateRange.end)),
+                  IconButton(
+                    icon: const Icon(Icons.calendar_today_outlined),
+                    highlightColor: Colors.cyan,
+                    color: Colors.cyan,
+                    onPressed: () {
+                      _pickToDate();
+                    },
+                  )
+                ],
+              ),
+              Row(
+                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    child: const Text("Content"),
+                    width: 100,
+                    margin: const EdgeInsets.only(left: 80),
+                  ),
+                  DropdownButton(
+                    value: _contentSelected,
+                    items: contentItems.map((String contentItems) {
+                      return DropdownMenuItem(
+                          value: contentItems, child: Text(contentItems));
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _contentSelected = newValue!;
+                      });
+                    },
+                  )
+                ],
+              ),
+              Row(
+                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    child: const Text("Format"),
+                    width: 100,
+                    margin: const EdgeInsets.only(left: 80),
+                  ),
+                  DropdownButton(
+                    value: _formatSelected,
+                    items: formatItems.map((String formatItems) {
+                      return DropdownMenuItem(
+                          value: formatItems, child: Text(formatItems));
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _formatSelected = newValue!;
+                      });
+                    },
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: TextButton(
                       child: const Text("Generate",
-                          style: TextStyle(fontWeight: FontWeight.bold)),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20)),
                       onPressed: () {},
-                    )),
-                  ],
-                ),
-              ],
-            ),
+                    ),
+                  )
+                ],
+              ),
+            ],
           ),
         ));
+  }
+
+  _pickFromDate() async {
+    DateTime? newStart = await showDatePicker(
+      context: context,
+      firstDate: DateTime(_today.year - 5),
+      lastDate: DateTime(_today.year + 5),
+      initialDate: _dateRange.start,
+    );
+    DateTime end = _dateRange.end; // the present To date
+    if (end.difference(newStart!) >= const Duration(days: 0)) {
+      _dateRange = DateTimeRange(start: newStart, end: end);
+      setState(() {
+        _periodSelected = 'Other'; // to redraw the screen
+      });
+    } else {
+      _showAlertDates();
+    }
+  }
+
+  _pickToDate() async {
+    DateTime? newEnd = await showDatePicker(
+      context: context,
+      firstDate: DateTime(_today.year - 5),
+      lastDate: DateTime(_today.year + 5),
+      initialDate: _dateRange.end,
+    );
+    DateTime start =_dateRange.start; // the present To date
+    if (newEnd!.difference(start) >= const Duration(days: 0)) {
+      _dateRange = DateTimeRange(start: start, end: newEnd);
+      setState(() {
+        _periodSelected = 'Other'; // to redraw the screen
+      });
+    } else {
+      _showAlertDates();
+    }
+  }
+
+  void _showAlertDates() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+              title: const Text("Range Dates"),
+              content: const Text(
+                  "The From date is after the To Date.\n\n Please, select a new date."),
+              actions: [
+                TextButton(
+                    onPressed: () => Navigator.pop(context, 'OK'),
+                    child: const Text(
+                      "ACCEPT",
+                      style: TextStyle(fontSize: 25),
+                    ))
+              ],
+            ));
   }
 }
